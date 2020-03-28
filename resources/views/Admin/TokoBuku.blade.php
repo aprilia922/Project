ce4502b06aa24b8c71d8d91097b27a47f2cb6826
aprilia922 <kdapriliacahyani@gmail.com>
2020-03-28 10:25:36 +0800

    first commit

---
+++ resources/views/Admin/TokoBuku.blade.php
@@ -0,0 +1,56 @@
+@extends('Layout.layouts')
+
+@section('content')
+<div class="main-content">
+        <div class="container-fluid">
+          <!-- OVERVIEW -->
+          <div class="panel panel-headline">
+            <div class="panel-heading">
+              <h3 class="panel-title">Beranda</h3>
+              <p class="panel-subtitle">Hari/Tanggal : Jumat, 27 Maret 2020</p>
+            </div>
+            <div class="panel-body">
+              
+                        <div class="row">
+            <div class="col-md-6">
+              <!-- RECENT PURCHASES -->
+              <div class="panel">
+                <div class="panel-heading">
+                  <h3 class="panel-title">BIODATA</h3>
+                </div>
+                <div class="panel-body">
+                                    <p>NAMA     : Kadek Aprilia Cahyani</p>
+                                    <P>NIM      : 1815051019</P>
+                                    <P>KELAS    : PTI 4D</P>
+                                    <P>PRODI    : Pendidikan Teknik Informatika</P>
+                                    <P>JURUSAN  : Teknik Informatika</P>
+                                    <P>FAKULTAS : Teknik dan Kejuruan</P>
+                                </div>
+                
+              </div>
+              <!-- END RECENT PURCHASES -->
+            </div>
+            <div class="col-md-6">
+              <!-- MULTI CHARTS -->
+              <div class="panel">
+                <div class="panel-heading">
+                  <h3 class="panel-title">STUDI KASUS</h3>
+                </div>
+                <div class="panel-body">
+                                    <p>JUDUL        : Sistem Pelayanan Toko Buku Rahayu</p>
+                                    <p>PENJELASAN   : Sistem ini memudahkan pembeli untuk membeli dengan mencari buku yang disukai dan memudahkan penjual dalam memanajemen penjualan</p>
+                
+                </div>
+              </div>
+              <!-- END MULTI CHARTS -->
+            </div>
+          </div>
+            </div>
+          </div>
+          <!-- END OVERVIEW -->
+          
+          
+          
+        </div>
+      </div>
+@endsection('content')
